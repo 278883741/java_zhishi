cba4735e3c0199042d8ef081675cccf5efc3426d
zhaojianfei YS2349 (Partner) <YS.zhaojianfei@h3c.com>
2019-05-07 13:52:47 +0800

    121

--- hearthstone_card_rarity.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO hearthstone.card_rarity (Name) VALUES ('普通');
-INSERT INTO hearthstone.card_rarity (Name) VALUES ('免费');
-INSERT INTO hearthstone.card_rarity (Name) VALUES ('稀有');
-INSERT INTO hearthstone.card_rarity (Name) VALUES ('史诗');
-INSERT INTO hearthstone.card_rarity (Name) VALUES ('传说');
--- hearthstone_card_type.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO hearthstone.card_type (Name) VALUES ('仆从');
-INSERT INTO hearthstone.card_type (Name) VALUES ('法术');
-INSERT INTO hearthstone.card_type (Name) VALUES ('武器');
-INSERT INTO hearthstone.card_type (Name) VALUES ('英雄');
